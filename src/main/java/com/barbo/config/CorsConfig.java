2277060922bbd9640f2ad7750c13dcd3c8e1165d
Himanshu Tiwari <147228399+himanshuu2611@users.noreply.github.com>
2026-04-16 21:38:57 +0530

    Add package declaration to CorsConfig.java

--- src/main/java/com/barbo/config/CorsConfig.java
+++ src/main/java/com/barbo/config/CorsConfig.java
@@ -1,3 +1,4 @@
+package com.barbo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
