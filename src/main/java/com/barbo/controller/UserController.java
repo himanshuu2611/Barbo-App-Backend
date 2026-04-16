a59607900c5e0e5ef062439fc37c18cfd1ffe6a9
Himanshu Tiwari <147228399+himanshuu2611@users.noreply.github.com>
2026-04-16 21:01:17 +0530

    Update UserController.java

--- src/main/java/com/barbo/controller/UserController.java
+++ src/main/java/com/barbo/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173") // ✅ frontend access
+@CrossOrigin(origins = "https://barbo-app-frontend-salf.vercel.app") // ✅ frontend access
 public class UserController {
 
     private final UserService userService;
